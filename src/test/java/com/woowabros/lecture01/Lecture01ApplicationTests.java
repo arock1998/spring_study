d5f21f87df6bc4c23d2d2a873df31ee54c594af0
DESKTOP-D5R8E5M\arock <arock1998@naver.com>
2021-12-20 21:25:38 +0900

    초기 환경 세팅

---
+++ src/test/java/com/woowabros/lecture01/Lecture01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woowabros.lecture01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lecture01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
